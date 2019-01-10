363ba8805305baf88cd78e34f4cb0d4ac328c9b0
Barbara Almonte <almonte@134.sub-97-32-138.myvzw.com>
2019-01-10 12:03:37 -0500

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar';
-import VideoList from './components/video_list';
 import VideoDetail from './components/video_details';
+//import "../style/style.css"
 
 const API_KEY = "AIzaSyC1B-oVHoektfjZRJn26MTxi4KKRvNzxwM";
 
@@ -14,23 +13,19 @@ class App extends Component{
 
         this.state = { videos: []};
 
-        YTSearch({key: API_KEY, term: 'goku vs. jiren'}, (videos) => {this.setState({videos});});
+        YTSearch({key: API_KEY, term: 'fanfare trumpet victory tune sound effect hd'}, (videos) => {this.setState({videos});});
     // this.setState({videos:videos});
     }
 
     render(){
         return (
-        <div>
-            <SearchBar/>
+        <div className="video-player">
             <VideoDetail video = {this.state.videos[0]}/>
-            <VideoList videos = {this.state.videos}/>
-
         </div>
     );
     }
 
 }
-
 
 
 ReactDOM.render(<App/>, document.querySelector(".container"));
