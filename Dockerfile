07d4236ef8252d50d3749444c880caf8ea3739de
Barbara Almonte <almonte@134.sub-97-32-138.myvzw.com>
2019-01-10 17:28:23 -0500

    added post call to test response when dockerized

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # Stage 0, "build-stage", based on Node.js, to build and compile the frontend
 FROM tiangolo/node-frontend:10 as build-stage
 WORKDIR /app
-COPY package*.json /app/
+COPY package.json /app/
 RUN npm install
 COPY ./ /app/
 RUN npm run build
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './style/style.css'
 
 
-class App extends React.Component {
+class App extends Component {
 
     constructor(props){
         super(props);
@@ -14,15 +13,42 @@ class App extends React.Component {
 
     handleSearch = (user) =>{
         let url = 'https://api.github.com/users/'+user+'/repos';
-        fetch(url).
-        then(response => response.json()).then((repos) => {
+        fetch(url)
+            .then(response => response.json()).then((repos) => {
             console.log(repos);
             console.log(repos.length);
             this.setState({
                 repos: repos
             });
+        }).catch(() => {
+            console.log("User not found");
         });
     };
+
+    options;
+    returnsPost(){
+        this.options = {
+            method: 'POST',
+            data: {
+                title:'foo',
+                body: 'bar',
+                userId:1
+            },
+            credentials: 'include',
+            headers: {}
+        };
+        fetch('https://jsonplaceholder.typicode.com/posts', this.options)
+            .then((response) => {
+                return response.json();
+            })
+            .then((jsonObject) => {
+                console.log(jsonObject)
+                console.log(`ID ${jsonObject.id} was created. POST SUCCEEDED`);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
     render(){
         return (
@@ -30,15 +56,13 @@ class App extends React.Component {
                 <h3>C'mon, stalk on someone's repo</h3>
                 <SearchBar handleSubmit={this.handleSearch} />
                 <RepoList repos={this.state.repos}/>
+                <div>{this.returnsPost()}</div>
             </div>
         )
     }
 }
 
-class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+class SearchBar extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
@@ -61,10 +85,10 @@ class SearchBar extends React.Component {
 }
 
 
-class RepoList extends React.Component {
+class RepoList extends Component {
 
     render(){
-        var rows = [];
+        let rows = [];
         this.props.repos.map((repo,index) => rows.push(<RepoItem key={index} repo={repo} />))
         return (
             <div className="list-group">
@@ -77,10 +101,10 @@ RepoList.defaultProps = {
     repos: []
 };
 
-class RepoItem extends React.Component {
+class RepoItem extends Component {
     render(){
         return (
-            <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+            <a href="/" className="list-group-item list-group-item-action flex-column align-items-start">
                 <div className="d-flex w-100 justify-content-between">
                     <h5 className="mb-1">{this.props.repo.name}</h5>
                     <small>{new Date(Date.parse(this.props.repo.created_at)).toLocaleDateString()}</small>
@@ -91,5 +115,6 @@ class RepoItem extends React.Component {
         )
     }
 }
+
 
 ReactDOM.render(<App/>, document.querySelector(".container"));
